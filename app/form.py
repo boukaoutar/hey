0d660e85f1a529af84f6dfab0e4036fb15e89417
boukaoutar <boulguidkaoutar@gmail.com>
2021-06-24 11:31:54 +0200

    new commit for requirements

--- app/form.py
+++ app/form.py
@@ -1,16 +1,6 @@
 from django.forms import ModelForm
 from .models import Upload
 
-#########
-import cv2
-import numpy as np
-import pandas as pd
-
-from shapely.geometry.polygon import Polygon
-from shapely.geometry import Point
-
-import imutils
-
 class UploadForm(ModelForm):
     class Meta:
         model = Upload
